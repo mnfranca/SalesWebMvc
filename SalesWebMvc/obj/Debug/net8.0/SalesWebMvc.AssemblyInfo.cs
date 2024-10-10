bf4f81d7096da59151dfd76c4db8efdb3d69e354
Marcelo <mnfranca@gmail.com>
2024-10-10 06:40:07 -0400

    Ajustes de versões

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfeed2562f8c7e3e46c83f74c33b6975ed70bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
