c1c88daca75a954fd10dbd6c01dcfacdcd889f95
Marcelo <mnfranca@gmail.com>
2024-10-11 18:59:05 -0400

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -36,4 +36,28 @@ public class SellersController : Controller
     _sellerService.Insert(seller);
     return RedirectToAction(nameof(Index));
   }
+
+  public IActionResult Delete(int? id)
+  {
+    if (id == null)
+    {
+      return NotFound();
+    }
+
+    var obj = _sellerService.FindById(id.Value);
+    if (obj == null)
+    {
+      return NotFound();
+    }
+
+    return View(obj);
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult Delete(int id)
+  {
+    _sellerService.Remove(id);
+    return RedirectToAction(nameof(Index));
+  }
 }
